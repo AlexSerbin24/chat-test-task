78037d9fc0c2dbbdee853d0121438b64664e8a9f
AlexSerbin24 <alex24072003s@gmail.com>
2024-08-13 15:16:21 +0300

    add frontend url in server

--- server/src/main.ts
+++ server/src/main.ts
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 }));
@@ -35,7 +35,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST'],
     credentials: true,
   }
